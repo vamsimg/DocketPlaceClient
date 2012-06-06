26d00376c381987a280bfe9e72c0c9386196b00b
Vamsi <vamsi.mg@gmail.com>
2012-06-06 19:31:35 +1000

    redirector added

---
+++ Redirector/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.IO.Ports;
+
+namespace Redirector
+{
+	class Program
+	{
+		public static void Main(String[] args)
+		{
+
+			String pipedText = "";
+			bool isKeyAvailable;
+
+			try
+			{
+				isKeyAvailable = System.Console.KeyAvailable;
+			}
+			catch (InvalidOperationException expected)
+			{
+				pipedText = System.Console.In.ReadToEnd();
+			}
+			pipedText = pipedText.Replace("\n\n\n\n\n\n\n\n", "");
+			//do something with pipedText or the args
+			// Instantiate the communications
+			// port with some basic settings
+			SerialPort port = new SerialPort(
+			  args[0], 9600, Parity.None, 8, StopBits.One);
+
+			// Open the port for communications
+			port.Open();
+
+			// Write a string
+			port.Write(pipedText);
+
+			// Close the port
+			port.Close();
+		}
+	}
+}
